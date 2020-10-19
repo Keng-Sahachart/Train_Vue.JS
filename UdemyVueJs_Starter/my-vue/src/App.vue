1f38aaf43a1b82a97262298423224746d76af2eb
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-10-19 12:39:16 +0700

    UP

--- UdemyVueJs_Starter/my-vue/src/App.vue
+++ UdemyVueJs_Starter/my-vue/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>|
+    <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
-  <Footer/>
+  <Footer />
 </template>
 
 <script>
 import Footer from "@/components/Footer.vue";
 export default {
   components: {
-    Footer,
-  },
+    Footer
+  }
 };
 </script>
 
