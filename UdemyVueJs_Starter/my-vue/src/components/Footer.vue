eb533bcab1d56aa06b951663bc8e76f99eb43e51
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-10-19 09:39:52 +0700

    Revert "Revert "how to use data()""
    This reverts commit 870b3c40e976ffbbed8ee843a5f66ecc1ef7474d.

---
+++ UdemyVueJs_Starter/my-vue/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>CopyRight {{ currentYear }} by Sahachart</div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  // data Object
+  data() {
+    return {
+      currentYear: new Date().getFullYear() + 1
+    };
+  }
+};
+</script>
